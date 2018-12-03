ee536e4b420cdf15ea4dbfc1d68e607aaebcac69
saber <350224276@qq.com>
2018-12-03 13:43:02 +0800

    [+]first

---
+++ src/Interfaces/Upload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saber
+ * Date: 2018/12/1
+ * Time: 14:10
+ */
+
+namespace saber\interfaces;
+
+interface Upload
+{
+    public function upload($file);
+}
